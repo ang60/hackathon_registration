1664487a064ade2c5dbfece42b967ee272d579cf
angella ngina <angelngina98@gmail.com>
2023-11-08 15:18:26 +0300

    fixed errors

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 
 # registration/urls.py
 from django.urls import path
-from . import views
+from views import*
 
 urlpatterns = [
-    path('register/', views.register_participant, name='register_participant'),
-    path('registration_success/', views.registration_success, name='registration_success'),
+    path('register/',register_participant, name='register_participant'),
+    path('registration_success/',registration_success, name='registration_success'),
 ]
